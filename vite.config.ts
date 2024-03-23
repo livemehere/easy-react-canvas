4b714ed8a5b1453fb6b2cad7344f4dbba164c1ee
livemehere <rhdxoals@gmail.com>
2024-03-23 21:29:25 +0900

    📝 readme update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      include: "src/lib/**/*",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: "src/lib/index.ts",
+      name: "easy-react-canvas",
+      fileName: "index",
+      formats: ["es", "cjs"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
